c675d7b70606e5ade1db9028789fb2e93db9fa63
张亚飞 <672233580@qq.com>
2022-03-31 16:05:33 +0800

    Initial Commit

---
+++ MovieBanner/MovieBannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBannerApp.swift
+//  MovieBanner
+//
+//  Created by 张亚飞 on 2022/3/31.
+//
+
+import SwiftUI
+
+@main
+struct MovieBannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
